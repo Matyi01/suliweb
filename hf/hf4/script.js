93f05dea10d44ad845e47bd810e25df6958e75be
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2023-11-22 18:19:18 +0100

    asd

---
+++ hf/hf4/script.js
@@ -0,0 +1,24 @@
+function eger1(){
+    document.getElementById("1").style.backgroundColor = "red";
+}
+function eger2(){
+    document.getElementById("2").style.backgroundColor = "red";
+}
+function eger3(){
+    document.getElementById("3").style.backgroundColor = "red";
+}
+function eger4(){
+    document.getElementById("4").style.backgroundColor = "red";
+}
+function eger5(){
+    document.getElementById("5").style.backgroundColor = "red";
+}
+function eger6(){
+    document.getElementById("6").style.backgroundColor = "red";
+}
+function eger7(){
+    document.getElementById("7").style.backgroundColor = "red";
+}
+function eger8(){
+    document.getElementById("8").style.backgroundColor = "red";
+}
