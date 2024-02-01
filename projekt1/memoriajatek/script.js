010ab5612dbc924b5cee1d2a05ae55910eb6efcd
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2024-02-01 08:40:31 +0100

    asd

--- projekt1/memoriajatek/script.js
+++ projekt1/memoriajatek/script.js
@@ -27,8 +27,16 @@ function kepkirakas()
             let uj = document.createElement("div");
             uj.className = "kartya";
             uj.onclick = function(){
-                uj.style.backgroundImage = "url(kepek/" + kepekurl[i] + ")";
+                if (kattintas < 2)
+                {
+                    uj.style.backgroundImage = "url(kepek/" + kepekurl[i] + ")";
+                    uj.dataset.felforditva = "true";
+                }
                 kattintas++;
+                if (kattintas === 2)
+                {
+                    setTimeout(visszafordit, 2000);
+                }
             };
             
             kartyak.push(uj);
@@ -43,8 +51,61 @@ function kepkirakas()
     }
 }   
 
+function visszafordit()
+{
+    const lapok = document.getElementById("asztal").children;
+    const aktiv = [];
+    for (let i = 0; i < lapok.length; i++)
+    {
+        if (lapok[i].dataset.felforditva === "true")
+        {
+            aktiv.push(lapok[i]);
+        }
+    }
+    if (aktiv[0].style.backgroundImage !== aktiv[1].style.backgroundImage)
+    {
+        aktiv[0].style.backgroundImage = "";
+        aktiv[1].style.backgroundImage = "";
+    }
+    else
+    {
+        aktiv[0].onclick = "";
+        aktiv[1].onclick = "";
+        if (!vanemeg())
+        {
+            nyertel();
+        }
+    }
+    aktiv[0].dataset.felforditva = "";
+    aktiv[1].dataset.felforditva = "";
 
-function kever(points) {
+    kattintas = 0;
+}
+
+function nyertel()
+{
+    let uj = doucment.createElement("div");
+    uj.innerHTML = "Game over";
+
+    
+}
+
+function vanemeg()
+{
+    let db = 0;
+    const lapok = document.getElementById("asztal").children;
+    for (let i = 0; i < lapok.length; i++)
+    {
+        if (lapok[i].style.backgroundImage === "")
+        {
+            db++;
+        }
+    }
+    return db > 0;
+}
+
+function kever(points) 
+{
   for (let i = points.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i+1));
     let k = points[i];
