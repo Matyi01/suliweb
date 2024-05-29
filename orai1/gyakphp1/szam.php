8e05cd60c3fbd20f4976b3ac52f67b6f913205f4
Matyi01 <holczer.matyas.21@ady-nagyatad.hu>
2024-05-29 14:01:03 +0200

    asd

--- orai1/gyakphp1/szam.php
+++ orai1/gyakphp1/szam.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-    
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Szám: <input type="number" name="szam">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $szam = htmlspecialchars($_POST['szam']);
+  if (empty($szam)) {
+    echo "Nincs szám";
+  } else {
+    echo $szam * 2;
+  }
+}
+?>
 </body>
 </html>
