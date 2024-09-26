9d1285552e263e58073e740a41df8e5ec3972d73
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-09-26 10:30:20 +0200

    asd

--- BBB2024/tanolas1/script.js
+++ BBB2024/tanolas1/script.js
@@ -3,26 +3,34 @@ let inputsor = document.createElement("tr");
 let datuminputcella = document.createElement("td");
 let literinputcella = document.createElement("td");
 let forintinputcella = document.createElement("td");
+let kminputcella = document.createElement("td");
 
 let datuminput = document.createElement("input");
 let literinput = document.createElement("input");
 let forintinput = document.createElement("input");
+let kminput = document.createElement("input");
+
 
 datuminput.type = "date";
 literinput.type = "number";
 forintinput.type = "number";
+kminput.type = "number";
 
 datuminput.id = "datuminput";
 literinput.id = "literinput";
 forintinput.id = "forintinput";
+kminput.id = "kminput";
+
 
 datuminputcella.appendChild(datuminput);
 literinputcella.appendChild(literinput);
 forintinputcella.appendChild(forintinput);
+kminputcella.appendChild(kminput);
 
 inputsor.appendChild(datuminputcella);
 inputsor.appendChild(literinputcella);
 inputsor.appendChild(forintinputcella);
+inputsor.appendChild(kminputcella);
 
 document.getElementById("tankolasok").appendChild(document.createElement("tfoot")).appendChild(inputsor);
 
@@ -31,6 +39,7 @@ let honap = 0;
 let nap = 0;
 let liter = 0;
 let forint = 0;
+let km = 0;
 let adatok = [];
 let havibontas = [];
 
@@ -40,21 +49,26 @@ function hozzaad(){
     nap = document.getElementById("datuminput").value.split("-")[2];
     liter = document.getElementById("literinput").value;
     forint = document.getElementById("forintinput").value;
-    adatok.push([ev, honap, nap, liter, forint])
+    km = document.getElementById("kminput").value;
+
+    adatok.push([ev, honap, nap, liter, forint, km])
 
     let ujsor = document.createElement("tr")
 
     let datumcella = document.createElement("td");
     let litercella = document.createElement("td");
     let forintcella = document.createElement("td");
+    let kmcella = document.createElement("td");
 
     datumcella.innerHTML = ev + " " + honap + " " + nap;
     litercella.innerHTML = liter;
     forintcella.innerHTML = forint;
+    kmcella.innerHTML = km;
 
     ujsor.appendChild(datumcella);
     ujsor.appendChild(litercella);
     ujsor.appendChild(forintcella);
+    ujsor.appendChild(kmcella);
 
     document.getElementById("tankolasok").appendChild(ujsor);
 
@@ -75,14 +89,34 @@ function hozzaad(){
         }
     }
 
-
-    console.log(havibontas);
     document.getElementById("havikiadas").innerHTML = "";
 
     for (let i = 0; i < havibontas.length; i++){
-        for (let j = 0; j < 4; j++){
-            document.getElementById("havikiadas").innerHTML += havibontas[i][j] + " ";
+        document.getElementById("havikiadas").innerHTML += havibontas[i][0] + ". " + havibontas[i][1] + ". " + havibontas[i][2] + "L " + havibontas[i][3] + "Ft<br>";
+    }
+}
+function szures(){
+    let elejeev = document.getElementById("eleje").value.split("-")[0];
+    let elejehonap = document.getElementById("eleje").value.split("-")[1];
+    let elejenap = document.getElementById("eleje").value.split("-")[2];
+    let vegeev = document.getElementById("vege").value.split("-")[0];
+    let vegehonap = document.getElementById("vege").value.split("-")[1];
+    let vegenap = document.getElementById("vege").value.split("-")[2];
+    let kiirando = [];
+
+    for (let i = 0; i < adatok.length; i++){
+        if (adatok[i][0] >= elejeev && adatok[i][0] <= vegeev){
+            if (adatok[i][1] >= elejehonap && adatok[i][1] <= vegehonap){
+                if (adatok[i][2] >= elejenap && adatok[i][2] <= vegenap){
+                    kiirando.push(adatok[i]);
+                }
+            }
         }
-        document.getElementById("havikiadas").innerHTML += "<br>";
+    }
+
+
+    document.getElementById("szures").innerHTML = "";
+    for (let i = 0; i < kiirando.length; i++){
+        document.getElementById("szures").innerHTML += kiirando[i][0] + ". " + kiirando[i][1] + ". " + kiirando[i][2] + ". " + kiirando[i][3] + "L " + kiirando[i][4] + "Ft " + kiirando[i][5] + "Km<br>";
     }
 }
