c73ad72bc8650611a27ec071e9104daa3f181017
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2025-02-11 12:04:23 +0100

    asd

---
+++ orai2/jquery_webshop/script.js
@@ -0,0 +1,53 @@
+function kartyakeszit(e) {
+    let card = jQuery("<div>");
+    card.addClass("card");
+    card.css("width", "18rem")
+    card.attr("id", "card");
+
+    let img = jQuery("<img>");
+    img.addClass("card-img-top");
+    img.attr("alt", "Card image");
+    img.attr("id", "img");
+    img.attr("src", e.image)
+
+    let cardbody = jQuery("<div>");
+    cardbody.addClass("card-body");
+    cardbody.attr("id", "cardbody");
+
+    let cardcontainer = jQuery("<div>");
+    cardcontainer.addClass("card-container");
+    cardcontainer.attr("id", "cardcontainer");
+
+    let cardtitle = jQuery("<h3>");
+    cardtitle.addClass("card-title");
+    cardtitle.attr("id", "cardtitle");
+    cardtitle.html(e.title);
+
+    let cardtext = jQuery("<p>");
+    cardtext.addClass("card-text");
+    cardtext.attr("id", "cardtext");
+    cardtext.html(e.description);
+
+    let cardprice = jQuery("<h2>");
+    cardprice.addClass("card-price");
+    cardprice.attr("id", "cardprice");
+    cardprice.html(e.price + " $");
+
+    card.append(img);
+    cardcontainer.append(cardtitle);
+    cardcontainer.append(cardtext);
+    cardbody.append(cardcontainer);
+    cardbody.append(cardprice);
+    card.append(cardbody);
+    $("body").append(card);
+}
+$.ajax({
+    url: 'https://fakestoreapi.com/products',
+    dataType: 'json',
+    success: function (data) {
+        console.log(data);
+        for (const e of data) {
+            kartyakeszit(e);
+        }
+    }
+});
