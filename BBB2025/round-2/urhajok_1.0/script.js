bc16bebe000ba3594b65cc6e423a1bcb19b49553
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2025-10-11 19:55:25 +0200

    asd

---
+++ BBB2025/round-2/urhajok_1.0/script.js
@@ -0,0 +1,21 @@
+/*
+1) A feladatok, amik rátok várnak:
+    a) Az oldal betöltésekor generálódjon egy 12x12-es pálya, ahol a 7 űrhajó véletlenszerűen helyezkedik el. Amire figyelni kell:
+        i) Egyik űrhajó sem lóghat le a pályáról,
+        ii) Az űrhajók nem lehetnek szomszédosak (sem élben, sem áltóban),
+        iii) Mind a 7 típusból pontosan egy lehet, de a forgatás megengedett.
+    b) A felhasználó a pálya mezőire kattintva küldi ki a szondákat.
+        i) A szonda jelezze, ha az adott mezőn van űrhajó.
+    c) Ha a mezőn nincs űrhajó, akkor egy számot írjon oda, ami megmondja, hogy milyen közel van a legközelebbi űrhajó.
+        i) Csak a távolságot kell kiírni, az irányt nem tudja a szonda megmondani.
+        ii) A távolságot csak élszomszédos lépésekkel vizsgáljuk, nem átlósan.
+    d) A játéknak akkor van vége, ha a játékos minden űrhajó minden mezőjét megtalálta, vagyis mindegyikre lőtt szondát.
+    e) A játék folyamatosan jelezze ki a lépések számát.
+    f) Legyen egy Time Attack játékmód is, ahol a lépések száma korlátozott (pl. 20, de célszerű tesztelés után egy megfelelő értékre állítani)
+        i) Minden egyes űrhajó teljes felfedezése adjon valamennyi (pl. 5) extra lépést.
+        ii) Ezt a játékmódot a játékos el is tudja veszteni, ha kifut a lépésekből mielőtt mindent megtalálna.
+*/
+
+function startGame() {
+    
+}
